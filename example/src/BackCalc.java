a08f7247317a574406de6db0fffc25ca8be3b029
xiayefeng <xiayu_12@yeah.net>
2024-08-03 19:02:21 +0800

    add file

---
+++ example/src/BackCalc.java
@@ -0,0 +1,122 @@
+public class BackCalc {
+    int[] result = new int[8];//全局或成员变量,下标表示行,值表示queen存储在哪一列
+    public void cal8queens(int row) { // 调用方式：cal8queens(0);
+        if (row == 8) { // 8个棋子都放置好了，打印结果
+            printQueens(result);
+            return; // 8行棋子都放好了，已经没法再往下递归了，所以就return
+        }
+        for (int column = 0; column < 8; ++column) { // 每一行都有8中放法
+            if (isOk(row, column)) { // 有些放法不满足要求
+                result[row] = column; // 第row行的棋子放到了column列
+                cal8queens(row+1); // 考察下一行
+            }
+        }
+    }
+
+    private boolean isOk(int row, int column) {//判断row行column列放置是否合适
+        int leftup = column - 1, rightup = column + 1;
+        for (int i = row-1; i >= 0; --i) { // 逐行往上考察每一行
+            if (result[i] == column) return false; // 第i行的column列有棋子吗？
+            if (leftup >= 0) { // 考察左上对角线：第i行leftup列有棋子吗？
+                if (result[i] == leftup) return false;
+            }
+            if (rightup < 8) { // 考察右上对角线：第i行rightup列有棋子吗？
+                if (result[i] == rightup) return false;
+            }
+            --leftup; ++rightup;
+        }
+        return true;
+    }
+
+    private void printQueens(int[] result) { // 打印出一个二维矩阵
+        for (int row = 0; row < 8; ++row) {
+            for (int column = 0; column < 8; ++column) {
+                if (result[row] == column) System.out.print("Q ");
+                else System.out.print("* ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+
+    public static int knapsack3(int[] weight, int[] value, int n, int w) {
+        int[][] states = new int[n][w+1];
+        for (int i = 0; i < n; ++i) { // 初始化states
+            for (int j = 0; j < w+1; ++j) {
+                states[i][j] = -1;
+            }
+        }
+        states[0][0] = 0;
+        if (weight[0] <= w) {
+            states[0][weight[0]] = value[0];
+        }
+        for (int i = 1; i < n; ++i) { //动态规划，状态转移
+            for (int j = 0; j <= w; ++j) { // 不选择第i个物品
+                if (states[i-1][j] >= 0) states[i][j] = states[i-1][j];
+            }
+            for (int j = 0; j <= w-weight[i]; ++j) { // 选择第i个物品
+                if (states[i-1][j] >= 0) {
+                    int v = states[i-1][j] + value[i];
+                    if (v > states[i][j+weight[i]]) {
+                        states[i][j+weight[i]] = v;
+                    }
+                }
+            }
+        }
+        // 找出最大值
+        int maxvalue = -1;
+        for (int j = 0; j <= w; ++j) {
+            if (states[n-1][j] > maxvalue) maxvalue = states[n-1][j];
+        }
+        return maxvalue;
+    }
+
+    // items商品价格，n商品个数, w表示满减条件，比如200
+    public static void double11advance(int[] items, int n, int w) {
+        boolean[][] states = new boolean[n][3*w+1];//超过3倍就没有薅羊毛的价值了
+        states[0][0] = true;  // 第一行的数据要特殊处理
+        if (items[0] <= 3*w) {
+            states[0][items[0]] = true;
+        }
+        for (int i = 1; i < n; ++i) { // 动态规划
+            for (int j = 0; j <= 3*w; ++j) {// 不购买第i个商品
+                if (states[i-1][j] == true) states[i][j] = states[i-1][j];
+            }
+            for (int j = 0; j <= 3*w-items[i]; ++j) {//购买第i个商品
+                if (states[i-1][j]==true) states[i][j+items[i]] = true;
+            }
+        }
+
+        int j;
+        for (j = w; j < 3*w+1; ++j) {
+            if (states[n-1][j] == true) break; // 输出结果大于等于w的最小值
+        }
+        if (j == 3*w+1) return; // 没有可行解
+        for (int i = n-1; i >= 1; --i) { // i表示二维数组中的行，j表示列
+            if(j-items[i] >= 0 && states[i-1][j-items[i]] == true) {
+                System.out.print(items[i] + " "); // 购买这个商品
+                j = j - items[i];
+            } // else 没有购买这个商品，j不变。
+        }
+        if (j != 0) System.out.print(items[0]);
+    }
+
+    public static int knapsack2(int[] items, int n, int w) {
+        boolean[] states = new boolean[w+1]; // 默认值false
+        states[0] = true;  // 第一行的数据要特殊处理，可以利用哨兵优化
+        if (items[0] <= w) {
+            states[items[0]] = true;
+        }
+        for (int i = 1; i < n; ++i) { // 动态规划
+            for (int j = w-items[i]; j >= 0; --j) {//把第i个物品放入背包
+                if (states[j]==true) states[j+items[i]] = true;
+            }
+        }
+        for (int i = w; i >= 0; --i) { // 输出结果
+            if (states[i] == true) return i;
+        }
+        return 0;
+    }
+
+
+}
