49d6c23bc858cf6411a34cee9aac2f62db83f7d4
xiayefeng <xiayu_12@yeah.net>
2024-08-03 23:27:13 +0800

    add file

---
+++ chapter02/FloatDoubleExer1.java
@@ -0,0 +1,11 @@
+class FloatDoubleExer1 
+{
+	public static void main(String[] args){
+       
+	   double hua = 80.0;
+       
+	   double she = (hua - 32) / 1.8;
+
+	   System.out.println("华氏度"+ hua+"℉对应的摄氏度为"+she + "℃");	
+	}
+}
---
+++ example/src/Graph.java
@@ -0,0 +1,108 @@
+public class Graph {
+    private int v; // 顶点的个数
+    private LinkedList<Integer> adj[]; // 邻接表
+
+    public Graph(int v) {
+        this.v = v;
+        adj = new LinkedList[v];
+        for (int i=0; i<v; ++i) {
+            adj[i] = new LinkedList<>();
+        }
+    }
+
+    public void addEdge(int s, int t) { // s先于t，边s->t
+        adj[s].add(t);
+    }
+
+    public void addEdge(int s, int t, int w) { // 添加一条边
+        this.adj[s].add(new Edge(s, t, w));
+    }
+    private class Edge {
+        public int sid; // 边的起始顶点编号
+        public int tid; // 边的终止顶点编号
+        public int w; // 权重
+        public Edge(int sid, int tid, int w) {
+            this.sid = sid;
+            this.tid = tid;
+            this.w = w;
+        }
+    }
+    // 下面这个类是为了dijkstra实现用的
+    private class Vertex {
+        public int id; // 顶点编号ID
+        public int dist; // 从起始顶点到这个顶点的距离
+        public Vertex(int id, int dist) {
+            this.id = id;
+            this.dist = dist;
+        }
+    }
+
+    public void topoSortByKahn() {
+        int[] inDegree = new int[v]; // 统计每个顶点的入度
+        for (int i = 0; i < v; ++i) {
+            for (int j = 0; j < adj[i].size(); ++j) {
+                int w = adj[i].get(j); // i->w
+                inDegree[w]++;
+            }
+        }
+        LinkedList<Integer> queue = new LinkedList<>();
+        for (int i = 0; i < v; ++i) {
+            if (inDegree[i] == 0) queue.add(i);
+        }
+        while (!queue.isEmpty()) {
+            int i = queue.remove();
+            System.out.print("->" + i);
+            for (int j = 0; j < adj[i].size(); ++j) {
+                int k = adj[i].get(j);
+                inDegree[k]--;
+                if (inDegree[k] == 0) queue.add(k);
+            }
+        }
+    }
+
+    public void topoSortByDFS() {
+        // 先构建逆邻接表，边s->t表示，s依赖于t，t先于s
+        LinkedList<Integer> inverseAdj[] = new LinkedList[v];
+        for (int i = 0; i < v; ++i) { // 申请空间
+            inverseAdj[i] = new LinkedList<>();
+        }
+        for (int i = 0; i < v; ++i) { // 通过邻接表生成逆邻接表
+            for (int j = 0; j < adj[i].size(); ++j) {
+                int w = adj[i].get(j); // i->w
+                inverseAdj[w].add(i); // w->i
+            }
+        }
+        boolean[] visited = new boolean[v];
+        for (int i = 0; i < v; ++i) { // 深度优先遍历图
+            if (visited[i] == false) {
+                visited[i] = true;
+                dfs(i, inverseAdj, visited);
+            }
+        }
+    }
+
+    private void dfs(
+            int vertex, LinkedList<Integer> inverseAdj[], boolean[] visited) {
+        for (int i = 0; i < inverseAdj[vertex].size(); ++i) {
+            int w = inverseAdj[vertex].get(i);
+            if (visited[w] == true) continue;
+            visited[w] = true;
+            dfs(w, inverseAdj, visited);
+        } // 先把vertex这个顶点可达的所有顶点都打印出来之后，再打印它自己
+        System.out.print("->" + vertex);
+    }
+
+    HashSet<Integer> hashTable = new HashSet<>(); // 保存已经访问过的actorId
+    long findRootReferrerId(long actorId) {
+        if (hashTable.contains(actorId)) { // 存在环
+            return;
+        }
+        hashTable.add(actorId);
+        Long referrerId =
+                select referrer_id from [table] where actor_id = actorId;
+        if (referrerId == null) return actorId;
+        return findRootReferrerId(referrerId);
+    }
+
+}
+
