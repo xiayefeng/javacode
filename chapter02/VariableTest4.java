12915485b726920f4e03eb1582f8386fc657984e
xiayefeng <xiayu_12@yeah.net>
2024-08-04 23:48:35 +0800

    Create VariableTest4.java

---
+++ chapter02/VariableTest4.java
@@ -0,0 +1,16 @@
+class VariableTest4{
+  public static void main(String[] args){
+    
+	char c1='a';
+	char c2='中';
+	char c3='★';
+	
+	
+	char c4='\u0043';
+	char c5='\u0036';
+	System.out.println(c4);
+	System.out.println(c5);
+	
+  }
+
+}
