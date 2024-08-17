69434ae34d393220cbb3740dd5634b9759bd533e
xiayefeng <xiayu_12@yeah.net>
2024-08-17 10:08:47 +0800

    Update AriExer.java

--- chapter02/AriExer.java
+++ chapter02/AriExer.java
@@ -5,9 +5,9 @@ class AriExer {
 		int ge = num %10;
 		int shi = num / 10 % 10;
 		int bai = num /100;
-		System.out.println("��λ��:" + ge);
-		System.out.println("ʮλ��:" + shi);
-		System.out.println("��λ��:" + bai);
+		System.out.println("个位:" + ge);
+		System.out.println("十位:" + shi);
+		System.out.println("百位:" + bai);
 		System.out.println("Hello World!");
 	}
 }
